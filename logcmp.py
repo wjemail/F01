abed3ed72a27cb1fc2fc820cdc5eb8a873fe0e11
wjemail <wjemail@yahoo.com>
2017-04-12 16:45:33 -0700

    upload logcmp.py

---
+++ logcmp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys
+import gzip
+refline = ''
+i = 1
+fname = sys.argv[1]
+if fname.endswith('.gz'):
+    f = gzip.open(fname)
+else:
+    f = open(fname)
+    
+for eachline in f:
+    if eachline == refline:
+        print "duplicated %d" % i
+        print refline,
+        print eachline,
+        print "."*100
+        i = i + 1
+    else:
+            refline = eachline
+
+
